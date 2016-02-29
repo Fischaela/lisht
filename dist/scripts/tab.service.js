4c1a61f8b97d05712e53cda6661042781af9f6ff
Michaela Lehr <michaela.lehr@geildanke.com>
2016-02-29 00:11:53 +0000

    ADD comment

--- dist/scripts/tab.service.js
+++ dist/scripts/tab.service.js
@@ -19,44 +19,46 @@ System.register(['angular2/core'], function(exports_1) {
         execute: function() {
             TabService = (function () {
                 function TabService() {
-                    console.log('Hallo');
-                    chrome.browserAction.onClicked.addListener(function (tab) {
-                        var _bookmarksLocalStorage = JSON.parse(localStorage.getItem('GEILDANKE-lisht')), _bookmarksMockData = [
-                            {
-                                'hyperlinks': [
-                                    { 'name': 'Twitter', 'url': 'https ://twitter.com' },
-                                    { 'name': 'Github', 'url': 'https ://github.com' },
-                                    { 'name': 'Facebook', 'url': 'https ://facebook.com' }
-                                ]
-                            },
-                            {
-                                'hyperlinks': [
-                                    { 'name': 'Geildanke', 'url': 'https ://geildanke.com' },
-                                    { 'name': 'Sitepoint', 'url': 'https ://sitepoint.com' },
-                                    { 'name': 'Codepen', 'url': 'https ://codepen.com' }
-                                ]
-                            },
-                            {
-                                'hyperlinks': [
-                                    { 'name': 'Dribbble', 'url': 'https ://dribbble.com' },
-                                    { 'name': 'Designer News', 'url': 'https ://news.layervault.com' },
-                                    { 'name': 'Mail', 'url': 'https ://mail.google.com' }
-                                ]
+                    // check, if being extension or not
+                    if (typeof chrome.browserAction !== 'undefined') {
+                        chrome.browserAction.onClicked.addListener(function (tab) {
+                            var _bookmarksLocalStorage = JSON.parse(localStorage.getItem('GEILDANKE-lisht')), _bookmarksMockData = [
+                                {
+                                    'hyperlinks': [
+                                        { 'name': 'Twitter', 'url': 'https ://twitter.com' },
+                                        { 'name': 'Github', 'url': 'https ://github.com' },
+                                        { 'name': 'Facebook', 'url': 'https ://facebook.com' }
+                                    ]
+                                },
+                                {
+                                    'hyperlinks': [
+                                        { 'name': 'Geildanke', 'url': 'https ://geildanke.com' },
+                                        { 'name': 'Sitepoint', 'url': 'https ://sitepoint.com' },
+                                        { 'name': 'Codepen', 'url': 'https ://codepen.com' }
+                                    ]
+                                },
+                                {
+                                    'hyperlinks': [
+                                        { 'name': 'Dribbble', 'url': 'https ://dribbble.com' },
+                                        { 'name': 'Designer News', 'url': 'https ://news.layervault.com' },
+                                        { 'name': 'Mail', 'url': 'https ://mail.google.com' }
+                                    ]
+                                }
+                            ], _bookmarksToReturnArray = _bookmarksMockData;
+                            if (_bookmarksLocalStorage !== null) {
+                                _bookmarksToReturnArray = _bookmarksLocalStorage;
                             }
-                        ], _bookmarksToReturnArray = _bookmarksMockData;
-                        if (_bookmarksLocalStorage !== null) {
-                            _bookmarksToReturnArray = _bookmarksLocalStorage;
-                        }
-                        chrome.tabs.getSelected(null, function (tab) {
-                            var _newBookmarkName = {
-                                name: tab.title,
-                                url: tab.url
-                            }, _bookmarksToReturn = '';
-                            _bookmarksToReturnArray[0].hyperlinks.push(_newBookmarkName);
-                            _bookmarksToReturn = JSON.stringify(_bookmarksToReturnArray);
-                            localStorage.setItem('GEILDANKE-lisht', _bookmarksToReturn);
+                            chrome.tabs.getSelected(null, function (tab) {
+                                var _newBookmarkName = {
+                                    name: tab.title,
+                                    url: tab.url
+                                }, _bookmarksToReturn = '';
+                                _bookmarksToReturnArray[0].hyperlinks.push(_newBookmarkName);
+                                _bookmarksToReturn = JSON.stringify(_bookmarksToReturnArray);
+                                localStorage.setItem('GEILDANKE-lisht', _bookmarksToReturn);
+                            });
                         });
-                    });
+                    }
                 }
                 TabService = __decorate([
                     core_1.Injectable(), 
